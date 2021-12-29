2cc2dcc8a61343f61e390733a5ee0f7a19c6e4d5
Paul Shippy <pshippy@gmail.com>
2021-12-29 14:28:50 -0700

    add card to admin

--- battle/cards/models.py
+++ battle/cards/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Card(models.Model):
     name = models.CharField(max_length=200)
     hp = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.hp})"
